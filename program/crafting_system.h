5bd2ededa1b9015a0238e57a0886d41ba393e977
Maxim Slipenko <no-reply@maxim.slipenko.com>
2024-02-14 17:25:33 +0300

    добавлен crafting_system

---
+++ program/crafting_system.h
@@ -0,0 +1,9 @@
+// Система крафтинга - позволяет создавать новые предметы
+class CraftingSystem {
+public:
+    // Добавляет рецепт крафтинга
+    void addCraftingRecipe(/* параметры рецепта */);
+
+    // Крафтит предмет по рецепту
+    void craftItem(int recipeId);
+};
